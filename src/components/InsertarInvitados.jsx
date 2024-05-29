f21e6c0247b3205c72b362ba866e172f0a6bc95f
Pichu <luagostino@uade.edu.ar>
2024-05-29 19:31:51 -0300

    sacar link boludo

--- src/components/InsertarInvitados.jsx
+++ src/components/InsertarInvitados.jsx
@@ -1,7 +1,6 @@
 "use client"
 import { useState } from 'react';
 import { supabase } from '@/app/utils/supabase';
-import Link from 'next/link';
 
 const InsertarInvitados = ({ eventoID }) => {
     const [formData, setFormData] = useState({
