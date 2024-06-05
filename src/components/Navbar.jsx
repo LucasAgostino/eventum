46ab74018e55f3d688f3beb15164f31c95806de7
Agustin <agusbarralia@gmail.com>
2024-06-04 22:32:21 -0300

    diseño boton cerrar sesion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import { LockClosedIcon } from "@heroicons/react/solid";
-import { LockOpenIcon } from "@heroicons/react/solid";
 import { supabase } from "@/utils/supabase";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
@@ -14,7 +12,8 @@ function Navbar() {
   const noNavbarRoutes = ["/login", "/register", "/home"];
   const hideNavbarOnDynamicRoutes = pathname.startsWith("/confirm/");
 
-  const showNavbar = !noNavbarRoutes.includes(pathname) && !hideNavbarOnDynamicRoutes;
+  const showNavbar =
+    !noNavbarRoutes.includes(pathname) && !hideNavbarOnDynamicRoutes;
 
   const handleLogout = async () => {
     const { error } = await supabase.auth.signOut();
@@ -26,73 +25,77 @@ function Navbar() {
   };
 
   useEffect(() => {
-    const isEventoPage = pathname.startsWith("/eventos/") && pathname.split("/").length === 3;
+    const isEventoPage =
+      pathname.startsWith("/eventos/") && pathname.split("/").length === 3;
     setIsSpecificPage(isEventoPage);
   }, [pathname]);
 
   return (
-  <div>
-    {showNavbar && (
-      <nav className="bg-custom-blue p-4 flex justify-between items-center font-bold">
-        <div className="flex items-center">
-          <h1>
-            <Link href="/">
-              <img
-                src="/favicon.ico"
-                alt=""
-                style={{ width: "80px", height: "80px" }}
-              />
-            </Link>
-          </h1>
-        </div>
-        <div className="flex justify-center">
-          <ul className="flex space-x-4">
-            <li>
+    <div>
+      {showNavbar && (
+        <nav className="bg-custom-blue p-4 flex justify-between items-center font-bold">
+          <div className="flex items-center">
+            <h1>
               <Link href="/">
-                <span className="text-white hover:text-gray-300 cursor-pointer">
-                  Home
-                </span>
+                <img
+                  src="/favicon.ico"
+                  alt=""
+                  style={{ width: "80px", height: "80px" }}
+                />
               </Link>
-            </li>
-            <li>
-              <Link href="/eventos">
-                <span className="text-white hover:text-gray-300 cursor-pointer">
-                  Eventos
-                </span>
-              </Link>
-            </li>
-            {isSpecificPage && (
-              <>
-                <li>
-                  <Link href="/eventos/presupuesto">
-                    <div className="text-white hover:text-gray-300">
-                      Presupuestos
-                    </div>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/eventos/reportes">
-                    <div className="text-white hover:text-gray-300">
-                      Reportes
-                    </div>
-                  </Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
+            </h1>
+          </div>
+          <div className="flex justify-center">
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="/">
+                  <span className="text-white hover:text-gray-300 cursor-pointer">
+                    Home
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link href="/eventos">
+                  <span className="text-white hover:text-gray-300 cursor-pointer">
+                    Eventos
+                  </span>
+                </Link>
+              </li>
+              {isSpecificPage && (
+                <>
+                  <li>
+                    <Link href="/eventos/presupuesto">
+                      <div className="text-white hover:text-gray-300">
+                        Presupuestos
+                      </div>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/eventos/reportes">
+                      <div className="text-white hover:text-gray-300">
+                        Reportes
+                      </div>
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
 
-        <div className="flex items-center">
-          <button
-            onClick={handleLogout}
-            className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold shadow transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-hray-50 hover:bg-red-600 text-white dark:bg-gray-700 dark:text-white dark:text-white dark:hover:text-gray-200 dark:shadow-none group ml-2"
-          >
-            Cerrar sesion
-          </button>
-        </div>
-      </nav>
-    )}
-  </div>
+          <div className="flex items-center">
+            <button
+              onClick={handleLogout}
+              className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold shadow transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 hover:bg-red-600 text-gray-900 hover:text-white group ml-2"
+            >
+              <span className="absolute inset-0 bg-red-600 transition-all duration-150 ease-in-out transform -translate-x-full group-hover:translate-x-0"></span>
+              <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                Cerrar sesión
+              </span>
+            </button>
+          </div>
+        </nav>
+      )}
+    </div>
   );
 }
 
